4da3a21f27a790e648378888e6aa02b73ab8e186
yuuuh26 <dengana.10011212@gmail.com>
2026-09-22 01:27:10 -0700

    Bump cache for privacy shortcut update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'sorosoro-v1.1.2';
+const CACHE_NAME = 'sorosoro-v1.1.3';
 const APP_SHELL = [
   './', './index.html', './styles.css', './manifest.webmanifest',
   './js/app.js', './js/db.js', './js/date.js',
