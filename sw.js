a0ebabdc46c3323d2cac461c9849082a4cf15c63
yuuuh26 <dengana.10011212@gmail.com>
2026-09-22 01:18:15 -0700

    Bump cache after startup recovery fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'sorosoro-v1.1.1';
+const CACHE_NAME = 'sorosoro-v1.1.2';
 const APP_SHELL = [
   './', './index.html', './styles.css', './manifest.webmanifest',
   './js/app.js', './js/db.js', './js/date.js',
