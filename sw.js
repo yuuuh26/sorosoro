8a4804b38f98cc5658a15a09d421b56c759882f3
yuuuh26 <dengana.10011212@gmail.com>
2026-09-22 00:51:07 -0700

    Bump service worker cache for v1.1.0

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'sorosoro-v1.0.0';
+const CACHE_NAME = 'sorosoro-v1.1.0';
 const APP_SHELL = [
   './', './index.html', './styles.css', './manifest.webmanifest',
   './js/app.js', './js/db.js', './js/date.js',
